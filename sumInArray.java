7081ad432980e959dae0117b5cd3978ace410b9a
unknown <siddhantcs7c@gmail.com>
2023-08-31 00:07:39 +0530

    first day

---
+++ sumInArray.java
@@ -0,0 +1,28 @@
+//import java.lang.reflect.Array;
+
+import java.util.Arrays;
+
+class solution{
+    public static void sumfinder(int array[],int target){
+        Arrays.sort(array);
+        int low = 0;
+        int high = array.length-1;
+        while(low<high){
+            if((array[low] +  array[high])==target){
+                System.out.print("pair found "+ array[low] +" "+ array[high]);
+                return;
+            }
+            if((array[low] +  array[high])<target){
+                low++;
+            }
+            else{
+                high--;
+            }
+        }System.out.println("Pair not found");
+    }
+    public static void main(String[] args) {
+        int number[]={8,7,2,5,3,1};
+        int target = 10;
+        sumfinder(number, target);
+    }
+}
