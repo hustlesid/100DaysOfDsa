a2e9f1b09bdf2ebe706b45730953316cda7c9fb7
unknown <siddhantcs7c@gmail.com>
2023-09-04 22:49:51 +0530

    Kadane's Algorithm for subarray'sum

---
+++ kadaneAlgo.java
@@ -0,0 +1,25 @@
+ class kadaneAlgo {
+    public static int kadane(int array[]){
+        
+        int maxsum=0;
+        int currentsum=0;
+
+        for(int i=0;i<array.length;i++){
+            currentsum= currentsum+array[i];
+
+            if(currentsum>maxsum){
+                maxsum=currentsum;
+            }if(currentsum<maxsum){
+                currentsum = 0;
+            }
+        } return maxsum;
+        
+        
+    
+     // 
+    }
+    public static void main(String[] args) {
+        int array[]={5,-4,-2,6,-1};
+        System.out.println(kadane(array));
+    }
+}
